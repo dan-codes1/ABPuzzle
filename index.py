238d76f8de8780d8da30f467ebbf6781de310109
Daniel E <73743252+dan-codes1@users.noreply.github.com>
2025-01-26 14:27:06 -0800

    [Daniel] Added Bash Script (#3)
    * Create index.py
    
    * Update index.py
    
    * added bash script

--- index.py
+++ index.py
@@ -2,19 +2,41 @@ import sys
 import heapq
 import random
 
-class ABPuzzle:
-    def __init__(self, n: int, large_disks: [int], small_disks: [int]):
-        self.n = n
-        self.large_disks = large_disks
-        self.small_disks = small_disks
-        self.goal_state = self.compute_goal_state()
 
-    def compute_goal_state(self) -> [int]:
-        goal = []
-        for i in range(1, self.n + 1):
-            goal.extend([i] * self.n)
-        goal.append(0)
-        return goal
+class ABPuzzle:
+    def __init__(self, n: int):
+        self.n = n
+        self.large_disks = []
+        self.small_disks = []
+        self.goal_state = []
+        self.configure()
+
+    def configure(self):
+        def configure_disks(n: int) -> ([int], [int]):
+            large_disks = []
+            small_disks = []
+
+            for _ in range(n ** 2):
+                large_disks.append(random.randint(1, 4))
+            large_disks.append(random.randint(1, 4))  # n^2 + 1
+
+            for i in range(1, n + 1):
+                small_disks.extend([i] * n)
+            small_disks.append(0)  # The uncovered small disk
+
+            random.shuffle(small_disks)
+            random.shuffle(large_disks)
+            return (small_disks, large_disks)
+
+        def configure_goal_state(n: int) -> [int]:
+            goal_state = []
+            for i in range(1, n + 1):
+                goal_state.extend([i] * n)
+            goal_state.append(0)
+            return goal_state
+
+        self.small_disks, self.large_disks = configure_disks(n=self.n)
+        self.goal_state = configure_goal_state(n=self.n)
 
     def heuristic(self, state: [int]) -> int:
         sum = 0
@@ -23,92 +45,71 @@ class ABPuzzle:
                 sum += 1
         return sum
 
-    def get_neighbors(self, state: [int]) -> [[int]]:
-        zero_index = state.index(0)
-        k = self.large_disks[zero_index]
+    def get_possible_moves(self, state: [int]) -> [[int]]:
+        zero_idx = state.index(0)
+        k = self.large_disks[zero_idx]
         neighbors = []
 
         def swap_and_create(new_index: int):
             new_state = state[:]
             # swap the disks
-            new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index]
+            new_state[zero_idx], new_state[new_index] = new_state[new_index], new_state[zero_idx]
             neighbors.append(new_state)
 
-        swap_and_create((zero_index - k) % len(state))
-        swap_and_create((zero_index + k) % len(state))
-        swap_and_create((zero_index - 1) % len(state))
-        swap_and_create((zero_index + 1) % len(state))
+        swap_and_create((zero_idx - k) % len(state))
+        swap_and_create((zero_idx + k) % len(state))
+        swap_and_create((zero_idx - 1) % len(state))
+        swap_and_create((zero_idx + 1) % len(state))
         return neighbors
+
+    def get_path(self, parent_map: dict[(int), [int]], end_state: [int]) -> [[int]]:
+        path = []
+        curr = end_state
+        while curr is not None:
+            path.append(curr)
+            curr = parent_map[tuple(curr)]
+        path.reverse()
+        return path
 
     def solve(self) -> [[int]]:
         initial_state = self.small_disks
-        if initial_state == self.goal_state:
+        if initial_state == self.goal_state:  # trivial case.
             return [initial_state]
 
         priority_queue: [(int, [int])] = []  # min-heap for best first search
         heapq.heappush(priority_queue, (self.heuristic(initial_state), initial_state))
-
-        visited: set[tuple[int]] = set() # using tuples instead of arrays because they are hashable for sets
+        visited: set[tuple[int]] = set()  # using tuples instead of arrays because they are hashable for sets
         visited.add(tuple(initial_state))
 
-        # to help reconstruct the solution path. Key is the state, value is the parent of the state.
-        parent_map: dict[(int), [int]] = {tuple(initial_state): None}
+        # to help get the solution path. Key is the state, value is the parent of the state.
+        path_map: dict[(int), [int]] = {tuple(initial_state): None}
 
         while priority_queue:
-            _, current_state = heapq.heappop(priority_queue)
+            _, curr_state = heapq.heappop(priority_queue)
+            possible_moves = self.get_possible_moves(curr_state)
+            for move in possible_moves:
+                move_tuple = tuple(move)
+                if move_tuple not in visited:
+                    visited.add(move_tuple)
+                    path_map[move_tuple] = curr_state
+                    if move == self.goal_state:  # solved!
+                        return self.get_path(path_map, move)
+                    hamming_distance = self.heuristic(move)
+                    heapq.heappush(priority_queue, (hamming_distance, move))
 
-            for neighbor in self.get_neighbors(current_state):
-                neighbor_tuple = tuple(neighbor)
-                if neighbor_tuple not in visited:
-                    visited.add(neighbor_tuple)
-                    parent_map[neighbor_tuple] = current_state
-
-                    if neighbor == self.goal_state: # solved!
-                        return self.reconstruct_path(parent_map, neighbor)
-
-                    heapq.heappush(priority_queue, (self.heuristic(neighbor), neighbor))
         return None  # no solution
 
-    def reconstruct_path(self, parent_map: dict[(int), [int]], end_state: [int]) -> [[int]]:
-        path = []
-        current = end_state
-        while current is not None:
-            path.append(current)
-            current = parent_map[tuple(current)]
-        path.reverse()
-        return path
-
-
-def generate_disks(n: int) -> ([int], [int]):
-    large_disks = [random.randint(1, 4) for _ in range(n ** 2)]
-    large_disks.append(random.randint(1, 4))  # n^2 + 1
-
-    small_disks = []
-    for i in range(1, n + 1):
-        small_disks.extend([i] * n)
-    small_disks.append(0)  # The uncovered small disk
-    random.shuffle(small_disks)
-    random.shuffle(large_disks)
-    return large_disks, small_disks
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python index.py <number_of_disks>")
-        sys.exit(1)
-
     n = int(sys.argv[1])
-    large_disks, small_disks = generate_disks(n)
-
-    print("Large disks:", large_disks)
-    print("Small disks:", small_disks)
-
-    # solver = ABPuzzle(3, [1, 2, 3, 4, 1, 2, 3, 1, 2, 3], [1, 1, 1, 3, 2, 2, 3, 3, 0, 2])
-    solver = ABPuzzle(n, large_disks, small_disks)
-    solution = solver.solve()
+    puzzle = ABPuzzle(n)
+    solution = puzzle.solve()
+    print("Large disks:", puzzle.large_disks)
+    print("Small disks:", puzzle.small_disks)
     print()
     if solution:
         print("Solution is")
         for state in solution:
-            print(" ".join(map(str, state)))
+            print(state)
     else:
         print("No solution")
